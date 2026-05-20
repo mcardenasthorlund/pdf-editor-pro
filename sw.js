527760910c3bd8cb7784b2392c216a8ce37d08e1
Manuel Cárdenas Thorlund <mcardenasthorlund@users.noreply.github.com>
2026-05-20 09:05:19 +0200

    Forzar el uso de recursos en internet para el sw

--- sw.js
+++ sw.js
@@ -1,49 +1,19 @@
-const CACHE_NAME = 'pdf-editor-v1.2';
-const ASSETS = [
-  './',
-  './index.html',
-  './assets/css/styles.css',
-  './assets/js/logic.js',
-  './assets/icons/icon.svg',
-  'https://unpkg.com/pdf-lib/dist/pdf-lib.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.11.174/pdf.min.js'
-];
+const CACHE_NAME = 'pdf-editor-disabled'; // No utilizado
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(ASSETS))
-      .then(() => self.skipWaiting())
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
+        cacheNames.map(name => caches.delete(name))
       );
     })
   );
 });
 
-self.addEventListener('message', event => {
-  if (event.data === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const responseClone = response.clone();
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, responseClone);
-        });
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+  event.respondWith(fetch(event.request));
 });
